7d26cc385888ee09d4b1c3f1c9667c871167719a
dingwan <dingwan0331@gmail.com>
2022-06-22 01:45:02 +0900

    Add: oders.models/Order.item & Order.user

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from django.db import models
 
-from core.models        import TimeStampModel
+from core.models import TimeStampModel
 
 class Cart(models.Model):
     user     = models.ForeignKey('users.User', on_delete = models.CASCADE)
--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from core.models   import TimeStampModel
+from core.models import TimeStampModel
 
 class Category(models.Model):
     name = models.CharField(max_length = 100)
