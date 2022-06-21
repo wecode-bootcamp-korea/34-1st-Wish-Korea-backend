815994b5a5cfed8c7bc330bbe30189cba3f380f9
dingwan <dingwan0331@gmail.com>
2022-06-21 17:23:16 +0900

    Add: users/models.py add adress_Field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from core.time_stamp_modules import TimeStampModel
+
+class User(TimeStampModel):
+    username     = models.CharField(max_length = 50, unique = True)
+    password     = models.CharField(max_length = 300)
+    email        = models.CharField(max_length = 100, unique = True)
+    phone_number = models.CharField(max_length = 50, unique = True)
+    adress       = models.CharField(max_length = 200, null = True)
+    first_name   = models.CharField(max_length = 50)
+    last_name    = models.CharField(max_length = 50)
+    nick_name    = models.CharField(max_length = 50, unique = True, null = True)
+
+    class Meta:
+        db_table = 'users'
