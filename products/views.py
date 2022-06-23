64306539f49dc37966493c8487d220e0b32a9955
dingwan <dingwan0331@gmail.com>
2022-06-23 14:59:37 +0900

    WIP

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from products.models import Category, SubCategory
+
+class NavigatorView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        sub_categories = SubCategory.objects.all()
+        result  = []
+        result2 = []
+
+        for category in categories:
+            result2 = []
+            for sub_category in sub_categories.filter(category_id = category.id):
+                result2.append({
+                    'id' : sub_category.name,
+                    'name' : sub_category.name
+                })
+            result.append(
+                {'category' : category.id,
+                'name'      : category.name,
+                'sub_category' : result2
+                }
+            )
+
+        return JsonResponse({'result' : result}, status = 200)
