30067ad90a3c232a5732b821acc449f58c1f5c54
dingwan <dingwan0331@gmail.com>
2022-06-24 15:53:47 +0900

    Fix: 멘토 리뷰 반영

--- products/views.py
+++ products/views.py
@@ -1,11 +1,9 @@
 import json
-from unittest import result
 
 from django.http  import JsonResponse
 from django.views import View
 
 from products.models       import Product
-from core.token_validators import token_validator
 
 class CategoryView(View):
     def get(self, request):
@@ -26,30 +24,22 @@ class CategoryView(View):
         return JsonResponse({'result' : result}, status = 200)
 
 class ProductView(View):
-    @token_validator
-    def get(self, request):
+    def get(self, request, product_id):
         try:
-            product_id = request.GET['product_id']
             product    = Product.objects.get(id = product_id)
 
             result = {
-                'is_user'              : bool(request.user),
-                'product_id'           : product_id,
-                'name'                 : None,
-                'tag'                  : product.tag,
-                'image'                : [url for url in product.imgaeurl_set.all()],
-                'manual'               : product.manual,
-                'content'              : product.content,
-                'important_components' : [
+                'product_id' : product_id,
+                'name'       : product.name,
+                'tag'        : product.tag,
+                'image'      : [url for url in product.imgaeurl_set.all()],
+                'manual'     : product.manual,
+                'content'    : product.content,
+                'components' : [
                     {
-                        'id'   : component.component.id,
-                        'name' : component.component.name
-                    } for component in product.productcomponent_set.filter(important = True).all()
-                ],
-                'components'           : [
-                    {
-                        'id'   : component.id,
-                        'name' : component.name
+                        'id'        : component.id,
+                        'name'      : component.name,
+                        'important' : component.productcomponent_set.get(product_id = product_id).important
                     } for component in product.component.all()
                 ],
                 'products'             : [
@@ -61,22 +51,11 @@ class ProductView(View):
                     }for item in product.item_set.all()
                 ] 
             }
-
-            if len(product.item_set.all()) == 1:
-                result['name'] = product.name
-            else:
-                size_values = sorted([size_value.size.size_g for size_value in product.item_set.all()])
-                name        = product.name
-                
-                for i in size_values:
-                    name += '/' + str(i) + 'g'
-
-                name           = name.replace(f'{product.name}/', f'{product.name} ')
-                result['name'] = name
-        
-            
             
             return JsonResponse({'result' : result}, status = 200)
         
         except KeyError:
+            return JsonResponse({'message' : 'Key Error'}, status = 400)
+        
+        except Product.DoesNotExist:
             return JsonResponse({'message' : 'Invalid Product'}, status = 400)
