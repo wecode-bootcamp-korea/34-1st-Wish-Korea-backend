1741e1fa7af360acbe4778c988b6d498e9a53e00
dingwan <dingwan0331@gmail.com>
2022-06-25 13:26:03 +0900

    WIP

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,25 @@
 import json
+
 import jwt
 
 from django.http  import JsonResponse
 from django.views import View
 
-from orders.models import *
+from orders.models import Cart
 
 class CartView(View):
-    def get(self, requset):
-        
+    def post(self, requset):
+        try:
+            data     = json.loads(requset.body)
+            user_id  = requset.user
+            item_id  = data['item_id']
+            quantity = data['quantity']
+
+             Cart.objects.create(
+                user_id  = user_id,
+                item_id  = item_id,
+                quantity = quantity
+            )
+
+        except KeyError:
+            return JsonResponse({'message' : 'Key Error'}, stat
