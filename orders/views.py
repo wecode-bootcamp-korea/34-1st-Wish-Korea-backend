ff8c2d125e9aeb458151b373cc0ae5a18f63adb9
dingwan <dingwan0331@gmail.com>
2022-06-25 13:59:22 +0900

    WIP

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import jwt
+
+from django.http  import JsonResponse
+from django.views import View
+
+from orders.models import *
+
+class CartView(View):
+    def get(self, request): 
