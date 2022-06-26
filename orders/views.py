39968cb5eb240966416bfe357a1359365eb8109a
dingwan <dingwan0331@gmail.com>
2022-06-26 16:37:27 +0900

    WIP

--- orders/views.py
+++ orders/views.py
@@ -12,14 +12,12 @@ class CartView(View):
         try:
             data     = json.loads(requset.body)
             user_id  = requset.user
-            item_id  = data['item_id']
-            quantity = data['quantity']
+            item_id_list  = [i['item_id'] for i in data['items']]
+            quantity_list = [i['quantity'] for i in data['items']]
 
-            Cart.objects.create(
-                user_id  = user_id,
-                item_id  = item_id,
-                quantity = quantity
-            )
+            cart = Cart.objects.get_or_create(user_id = user_id, item_id = user_id)
+
+            return JsonResponse({'message' : 'Create'}, status = 201)
 
         except KeyError:
             return JsonResponse({'message' : 'Key Error'}, status = 400)
