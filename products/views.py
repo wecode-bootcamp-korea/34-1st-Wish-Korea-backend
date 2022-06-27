4bd7b62ba2d7236d69481343284b8d256a36f867
dingwan <dingwan0331@gmail.com>
2022-06-28 01:12:05 +0900

    Fix: 멘토리 리뷰 반영

--- products/views.py
+++ products/views.py
@@ -33,16 +33,24 @@ class ProductListView(View):
         try:
             if request.GET.get('category_id'):
                 category_id = request.GET['category_id']
-                q           = Q(product__category_id = category_id)
+                q           = Q(sub_category__category_id = category_id)
+                category    = Category.objects.get(id = category_id)
 
             if request.GET.get('sub_category_id'):
                 category_id = request.GET['sub_category_id']
-                q           = Q(product__sub_category_id = category_id)
+                q           = Q(sub_category_id = category_id)
+                category    = SubCategory.objects.get(id = category_id)
+            
+            # if request.GET.get('category_id'):
+                # products = Product.objects.filter(sub_category__category_id = request.GET['category_id'])
+# 
+            # if request.GET.get('sub_category_id'):
+                # products = Product.objects.filter(sub_category_id = request.GET['sub_category_id'])
 
             result = { 
                 'category' : None,
                 'products' : [
-                    {
+                    {   
                         'id'               : product.id,
                         'name'             : product.name,
                         'tag'              : product.tag,
@@ -52,10 +60,10 @@ class ProductListView(View):
                         'is_made_in_korea' : product.is_made_in_korea,
                         'is_sold_out'      : not product.item_set.exclude(stock__exact = 0).exists(),
                         'price'            : int(product.item_set.order_by('price')[0].price), 
-                        'image_url'        : [image.url for image in product.imgaeurl_set.all()]
+                        'image_url'        : [image.url for image in product.imageurl_set.all()]
                 } for product in Product.objects.filter(q)],
             }
-            category = Category.objects.get(q)
+
             category_information = {
                 'id'        : category.id,
                 'content'   : category.content,
@@ -65,6 +73,51 @@ class ProductListView(View):
             result['category'] = category_information
                 
             return JsonResponse({'result' : result}, status = 200) 
+        
+        except KeyError:
+            return JsonResponse({'message' : 'Key Error'}, status = 400)
+
+        except SubCategory.DoesNotExist:
+            return JsonResponse({'message' : 'Invalid Category'}, status = 400)
+        
+        except Category.DoesNotExist:
+            return JsonResponse({'message' : 'Invalid Category'}, status = 400)
+
+class ProductView(View):
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id = product_id)
+
+            result = {
+                'product_id' : product_id,
+                'name'       : product.name,
+                'tag'        : product.tag,
+                'image'      : [url for url in product.imgaeurl_set.all()],
+                'manual'     : product.manual,
+                'content'    : product.content,
+                'components' : [
+                    {
+                        'id'        : component.id,
+                        'name'      : component.name,
+                        'important' : component.productcomponent_set.get(product_id = product_id).important
+                    } for component in product.component.all()
+                ],
+                'items' : [
+                    {
+                        'id'     : item.id,
+                        'size_g' : item.size.size_g,
+                        'price'  : int(item.price),
+                        'stock'  : item.stock,
+                        'image'  : item.image_url
+                    }for item in product.item_set.order_by('size__size_g')
+                ] 
+            }
+            
+            return JsonResponse({'result' : result}, status = 200)
+        
+        except Product.DoesNotExist:
+            return JsonResponse({'message' : 'Invalid Product'}, status = 400)
+
 
 
             # if request.GET.get('category_id'):
@@ -102,46 +155,5 @@ class ProductListView(View):
             #             'image_url'        : [image.url for image in product.imgaeurl_set.all()]
             #         } for product in products],
             #     }    
-
-            return JsonResponse({'result' : result}, status = 200)
-        
-        except KeyError:
-            return JsonResponse({'message' : 'Key Error'}, status = 400)
-
-        except SubCategory.DoesNotExist:
-            return JsonResponse({'message' : 'Invalid Category'}, status = 400)
-
-class ProductView(View):
-    def get(self, request, product_id):
-        try:
-            product = Product.objects.get(id = product_id)
-
-            result = {
-                'product_id' : product_id,
-                'name'       : product.name,
-                'tag'        : product.tag,
-                'image'      : [url for url in product.imgaeurl_set.all()],
-                'manual'     : product.manual,
-                'content'    : product.content,
-                'components' : [
-                    {
-                        'id'        : component.id,
-                        'name'      : component.name,
-                        'important' : component.productcomponent_set.get(product_id = product_id).important
-                    } for component in product.component.all()
-                ],
-                'items' : [
-                    {
-                        'id'     : item.id,
-                        'size_g' : item.size.size_g,
-                        'price'  : int(item.price),
-                        'stock'  : item.stock,
-                        'image'  : item.image_url
-                    }for item in product.item_set.order_by('size__size_g')
-                ] 
-            }
-            
-            return JsonResponse({'result' : result}, status = 200)
-        
-        except Product.DoesNotExist:
-            return JsonResponse({'message' : 'Invalid Product'}, status = 400)
+            #
+            # return JsonResponse({'result' : result}, status = 200)
