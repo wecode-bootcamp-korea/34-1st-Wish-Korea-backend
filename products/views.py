3749cac9d89e01e2ad8c04e4e3fb0d6d6a97083e
dingwan <dingwan0331@gmail.com>
2022-06-27 17:56:49 +0900

    WIP

--- products/views.py
+++ products/views.py
@@ -32,7 +32,6 @@ class ProductListView(View):
         try:
             if request.GET.get('category_id'):
                 category_id = request.GET['category_id']
-                category    = Category.objects.get(id = category_id)
                 products    = Product.objects.filter(sub_category__category_id = category_id)
                 
                 if category_id:
@@ -68,28 +67,28 @@ class ProductListView(View):
                 
                 return JsonResponse({'result' : result}, status = 200) 
             
-            sub_category_id = request.GET['sub_category_id']
-            sub_category    = SubCategory.objects.get(id = sub_category_id)
-            products        = sub_category.product_set.all()
+            # sub_category_id = request.GET['sub_category_id']
+            # sub_category    = SubCategory.objects.get(id = sub_category_id)
+            # products        = sub_category.product_set.all()
             
-            result = {
-                'sub_cateogry_id' : sub_category.id,
-                'content'         : sub_category.content,
-                'image_url'       : sub_category.image_url, 
-                'products'        : [
-                    {
-                        'id'               : product.id,
-                        'name'             : product.name,
-                        'tag'              : product.tag,
-                        'is_new'           : product.is_new,
-                        'is_vegan'         : product.is_vegan,
-                        'is_only_online'   : product.is_only_online,
-                        'is_made_in_korea' : product.is_made_in_korea,
-                        'is_sold_out'      : not product.item_set.exclude(stock__exact = 0).exists(),
-                        'price'            : int(product.item_set.order_by('price')[0].price), 
-                        'image_url'        : [image.url for image in product.imgaeurl_set.all()]
-                    } for product in products],
-                }    
+            # result = {
+            #     'sub_cateogry_id' : sub_category.id,
+            #     'content'         : sub_category.content,
+            #     'image_url'       : sub_category.image_url, 
+            #     'products'        : [
+            #         {
+            #             'id'               : product.id,
+            #             'name'             : product.name,
+            #             'tag'              : product.tag,
+            #             'is_new'           : product.is_new,
+            #             'is_vegan'         : product.is_vegan,
+            #             'is_only_online'   : product.is_only_online,
+            #             'is_made_in_korea' : product.is_made_in_korea,
+            #             'is_sold_out'      : not product.item_set.exclude(stock__exact = 0).exists(),
+            #             'price'            : int(product.item_set.order_by('price')[0].price), 
+            #             'image_url'        : [image.url for image in product.imgaeurl_set.all()]
+            #         } for product in products],
+            #     }    
 
             return JsonResponse({'result' : result}, status = 200)
         
