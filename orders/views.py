27e1e3314929e4d5cc6560b4e33db13352f5e710
dingwan <dingwan0331@gmail.com>
2022-06-27 13:16:57 +0900

    WIP

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,30 @@
 import json
+from unicodedata import name
 
 import jwt
 
 from django.http  import JsonResponse
 from django.views import View
 
-from orders.models import *
+from orders.models         import Cart
+from core.token_dacorators import token_decorator
 
 class CartView(View):
-    def get(self, request): 
+    @token_decorator
+    def get(self, request):
+        user   = request.user
+        carts  = Cart.objects.filter(user = user)
+        result = {
+            'cart' : [
+                {
+                'cart_id'   : cart.id,
+                'item_id'   : cart.item_id,
+                'item_name' : cart.item.product.name,
+                'quantity'  : cart.quantity,
+                'price'     : cart.item.price,
+                'stock'     : cart.item.stock,
+                'image_url' : cart.item.product.image_url,
+                'size'      : [size.size_g for size in cart.item.size_set],
+                'sub_catgory_name' : cart.item.product.sub_category.name
+            }for cart in carts]
+        }
