66967dde53cb2033373c38ed7fd31a214bfddfca
dingwan <dingwan0331@gmail.com>
2022-06-27 13:33:16 +0900

    Create: orders.views.CartView.get

--- orders/views.py
+++ orders/views.py
@@ -14,18 +14,19 @@ class CartView(View):
     def get(self, request):
         user   = request.user
         carts  = Cart.objects.filter(user = user)
+
         result = {
-            'cart_id'  : cart.id,
-            'quantity' : cart.quantity,
-            'item'    : 
+            'cart' : [
                 {
+                    'cart_id'   : cart.id,
                     'item_id'   : cart.item_id,
                     'name'      : cart.item.product.name,
                     'price'     : cart.item.price,
                     'stock'     : cart.item.stock,
                     'image_url' : cart.item.product.image_url,
                     'size'      : [size.size_g for size in cart.item.size_set],
-                    'sub_catgory_name' : cart.item.product.sub_category.name
+                    'sub_catgory_name' : cart.item.product.sub_category.name}
+            ]for cart in carts
             }
-        for cart in carts
-        }
+        
+        return JsonResponse({'result' : result}, status = 200)
