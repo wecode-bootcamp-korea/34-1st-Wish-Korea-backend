fee132b3dc80708c610c36a1bcf9cf905d4ab526
dingwan <dingwan0331@gmail.com>
2022-06-28 01:22:06 +0900

    Fix: image_url

--- orders/views.py
+++ orders/views.py
@@ -12,21 +12,26 @@ from core.token_dacorators import token_decorator
 class CartView(View):
     @token_decorator
     def get(self, request):
-        user   = request.user
-        carts  = Cart.objects.filter(user = user)
+        try:
+            user   = request.user
+            carts  = Cart.objects.filter(user = user)
 
-        result = {
-            'cart' : [
-                {
-                    'cart_id'   : cart.id,
-                    'items'     : cart.item_id,
-                    'name'      : cart.item.product.name,
-                    'price'     : cart.item.price,
-                    'size'      : cart.item.size.size_g,
-                    'stock'     : cart.item.stock,
-                    'image_url' : cart.item.product.imageurl_set.all()[0],
-                    'sub_catgory_name' : cart.item.product.sub_category.name}
-            ]for cart in carts
-            }
-        
-        return JsonResponse({'result' : result}, status = 200)
+            result = {
+                'carts' : [
+                    {
+                        'cart_id'   : cart.id,
+                        'items'     : cart.item_id,
+                        'name'      : cart.item.product.name,
+                        'price'     : cart.item.price,
+                        'size'      : cart.item.size.size_g,
+                        'stock'     : cart.item.stock,
+                        'image_url' : [image.url for image in cart.item.product.imageurl_set.all()],
+                        'sub_catgory_name' : cart.item.product.sub_category.name}
+                ]for cart in carts
+                }
+
+            return JsonResponse({'result' : result}, status = 200)
+
+        except IndexError:
+            result['cart']['image_url'] = ''
+            return JsonResponse({'result' : result}, status = 400)
