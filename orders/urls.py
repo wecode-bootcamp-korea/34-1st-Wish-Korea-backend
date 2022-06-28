07bb1422200fe059d9277cbf867c9db127bf454f
dingwan <dingwan0331@gmail.com>
2022-06-28 17:30:01 +0900

    Fix: urls.py /cart -> /carts

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from orders.views import CartView, CartsView
 
 urlpatterns = [
-    path('/cart/<int:cart_id>', CartView.as_view()),
+    path('/carts/<int:cart_id>', CartView.as_view()),
     path('/carts', CartsView.as_view())
 ]
