3f36893622df76ae49bb0a24348548509ff815f2
dingwan <dingwan0331@gmail.com>
2022-06-29 01:32:48 +0900

    Fix: 멘토리뷰반영

--- orders/views.py
+++ orders/views.py
@@ -28,23 +28,3 @@ class CartView(View):
         
         except Cart.DoesNotExist:
             return JsonResponse({'message' : 'Invalid Cart'}, status = 400)
-
-class CartsView(View):
-    @token_decorator
-    def post(self, requset):
-        try:
-            items = json.loads(requset.body)['items']
-
-            for item in items:
-                cart, created       = Cart.objects.get_or_create(user_id  = request.user.id, item_id  = item["id"])
-                total_cart_quantity = Cart.objects.filter(item_id=6).aggregate(total_quantity=Sum('quantity'))["total_quantity"]
-
-                if cart.item.stock - total_cart_quantity < item["quantity"]:
-                    return JsonResponse({'message' : 'Out of stock'}, status = 400)
-
-                cart.quantity += item["quantity"]
-
-            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
-           
-        except KeyError:
-            return JsonResponse({'message' : 'Key Error'}, status = 400)
