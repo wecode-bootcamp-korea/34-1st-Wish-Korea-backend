8e2e17b47e20d7fa3a2f0a6dadcff0d6688e7184
dingwan <dingwan0331@gmail.com>
2022-06-29 01:00:46 +0900

    Fix: stock을 item.stock - 현재 장바구니에 담겨있는 해당 item의 quantity를 계산해서 반환

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
 import json
 
-from django.http      import JsonResponse
-from django.views     import View
-from django.db.models import Sum
+from django.http                import JsonResponse
+from django.views               import View
+from django.db.models           import Sum
+from django.db.models.functions import Coalesce
 
 from orders.models         import Cart
 from core.token_decorators import token_decorator
@@ -27,3 +28,25 @@ class CartsView(View):
            
         except KeyError:
             return JsonResponse({'message' : 'Key Error'}, status = 400)
+
+class CartsView(View):
+    @token_decorator
+    def get(self, request):
+        user   = request.user
+        carts  = Cart.objects.filter(user = user)
+        result = {
+            'carts' : [
+                {
+                    'cart_id'   : cart.id,
+                    'items'     : cart.item_id,
+                    'name'      : cart.item.product.name,
+                    'price'     : int(cart.item.price),
+                    'size'      : cart.item.size.size_g,
+                    'stock'     : cart.item.stock - Cart.objects.filter(item_id = cart.item.id).aggregate(stock = Coalesce(Sum('quantity'), 0))['stock'],
+                    'quantity'  : cart.quantity,
+                    'image_url' : [image.url for image in cart.item.product.imageurl_set.all()],
+                    'sub_catgory_name' : cart.item.product.sub_category.name
+            }for cart in carts]
+        }
+            
+        return JsonResponse({'result' : result}, status = 200)
