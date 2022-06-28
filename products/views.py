83b51606b07267265d26a7d67857cc9e6e66adb0
dingwan <dingwan0331@gmail.com>
2022-06-28 17:59:11 +0900

    Fix: 멘토리뷰반영

--- products/views.py
+++ products/views.py
@@ -2,20 +2,19 @@ import json
 
 from django.http      import JsonResponse
 from django.views     import View
-from django.db.models import Q
+from django.db.models import Count, Q
 
 from products.models import Category, SubCategory, Product
 
 
 class CategoryView(View):
     def get(self, request):
-        categories = Category.objects.all()
+        categories = Category.objects.all().annotate(product_counts=Count("subcategory__product__id"))
         result = [
             {
                 'category_id'    : category.id, 
                 'name'           : category.name,
-                'products_count' : category.subcategory_set.filter(
-                    product__sub_category__in = category.subcategory_set.all()).count(),
+                'products_count' : category.products.counts,
                 'sub_categories' : [
                     {
                         'id'             : sub_category.id,
@@ -31,21 +30,20 @@ class CategoryView(View):
 class ProductListView(View):
     def get(self, request):
         try:
-            if request.GET.get('category_id'):
-                category_id = request.GET['category_id']
-                q           = Q(sub_category__category_id = category_id)
-                category    = Category.objects.get(id = category_id)
-
-            if request.GET.get('sub_category_id'):
-                category_id = request.GET['sub_category_id']
-                q           = Q(sub_category_id = category_id)
-                category    = SubCategory.objects.get(id = category_id)
+            category_id     = request.GET.get('category_id')
+            sub_category_id = request.GET.get('sub_category_id')
             
-            # if request.GET.get('category_id'):
-                # products = Product.objects.filter(sub_category__category_id = request.GET['category_id'])
-# 
-            # if request.GET.get('sub_category_id'):
-                # products = Product.objects.filter(sub_category_id = request.GET['sub_category_id'])
+            q = Q()
+            
+            if category_id:
+                q       &= Q(sub_category__category_id = category_id)
+                category = Category.objects.get(id = category_id)
+            
+            if sub_category_id:
+                q       &= Q(sub_category_id = category_id)
+                category = SubCategory.objects.get(id = sub_category_id)
+            
+            products = Product.objects.filter(q)
 
             result = { 
                 'category' : None,
@@ -59,9 +57,10 @@ class ProductListView(View):
                         'is_only_online'   : product.is_only_online,
                         'is_made_in_korea' : product.is_made_in_korea,
                         'is_sold_out'      : not product.item_set.exclude(stock__exact = 0).exists(),
+                        'price'            : list(map(int,[item.price for item in product.item_set.order_by('price')])), 
                         'price'            : int(product.item_set.order_by('price')[0].price), 
                         'image_url'        : [image.url for image in product.imageurl_set.all()]
-                } for product in Product.objects.filter(q)],
+                } for product in products],
             }
 
             category_information = {
