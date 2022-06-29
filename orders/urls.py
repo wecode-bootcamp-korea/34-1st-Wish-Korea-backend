44a62e3213b5ca58e32a679d090baeae24040a2d
dingwan <dingwan0331@gmail.com>
2022-06-29 17:37:53 +0900

    Fix: urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,5 @@ from orders.views import CartView, CartsView
 urlpatterns = [
     path('/carts/<int:cart_id>', CartView.as_view()),
     path('/carts', CartsView.as_view()),
-    path('/cart', CartsView.as_view())
+    path('/cart', CartView.as_view())
 ]
