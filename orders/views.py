c6396d6ecfd560cb3e3bd301a4382d1b41bb5e7f
dingwan <dingwan0331@gmail.com>
2022-06-29 16:41:50 +0900

    Fix: 멘토리뷰반영 & quantity가 0일때 -1 요청 들어오면 에러메세지 처리

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 import json
+from telnetlib import STATUS
 
 from django.http      import JsonResponse
 from django.views     import View
@@ -14,8 +15,12 @@ class CartView(View):
             cart                = json.loads(requset.body)
             cart_obj            = Cart.objects.get(id = cart.get('cart_id'))
             total_cart_quantity = Cart.objects.filter(item_=cart_obj.item).aggregate(total_quantity=Sum('quantity'))["total_quantity"]
+            
             if cart.item.stock - total_cart_quantity < cart.get("quantity"):
-                    return JsonResponse({'message' : 'Out of stock'}, status = 400)
+                return JsonResponse({'message' : 'Out of stock'}, status = 400)
+            
+            if cart_obj.quantity + cart.get('quantity') < 0:
+                return JsonResponse({'message' : "Quantity Already Zero"}, status = 400)
             
             cart.quantity += cart.get('quantity')
             cart.save()
@@ -23,7 +28,7 @@ class CartView(View):
             return JsonResponse({'message' : 'SUCCESS'}, status = 201)
         
         except Cart.DoesNotExist:
-            return JsonResponse({'message' : 'Invalid Cart'}, status = 400)
+            return JsonResponse({'message' : 'Invalid Cart'}, status = 404)
             
 class CartsView(View):
     @token_decorator
