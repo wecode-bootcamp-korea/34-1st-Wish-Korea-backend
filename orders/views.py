f8edc9b2e3555ae65f0a2494a99db7e73c0ef80c
dingwan <dingwan0331@gmail.com>
2022-06-29 14:07:56 +0900

    WIP

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.http                import JsonResponse
 from django.views               import View
-from django.db.models           import Sum
+from django.db.models           import Sum, F
 from django.db.models.functions import Coalesce
 
 from orders.models         import Cart
@@ -33,7 +33,8 @@ class CartsView(View):
     @token_decorator
     def get(self, request):
         user   = request.user
-        carts  = Cart.objects.filter(user = user)
+        carts  = Cart.objects.filter(user = user).annotate(Sum('item__cart_set_quantity'))
+        print(carts[2].quantity__sum)
         result = {
             'carts' : [
                 {
@@ -42,7 +43,7 @@ class CartsView(View):
                     'name'      : cart.item.product.name,
                     'price'     : int(cart.item.price),
                     'size'      : cart.item.size.size_g,
-                    'stock'     : cart.item.stock - Cart.objects.filter(item_id = cart.item.id).aggregate(stock = Coalesce(Sum('quantity'), 0))['stock'],
+                    #'stock'     : cart.item.stock - cart.aggregate(stock = Coalesce(Sum('quantity'), 0))['stock'],
                     'quantity'  : cart.quantity,
                     'image_url' : [image.url for image in cart.item.product.imageurl_set.all()],
                     'sub_catgory_name' : cart.item.product.sub_category.name
